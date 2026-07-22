7ceac07d015c0c24ea8abe7e2fe151dff7a0b9ea
BRINDHALAKSHMI1209 <145132542+BRINDHALAKSHMI1209@users.noreply.github.com>
2026-07-22 22:00:55 +0530

    Add files via upload
    Financial Forecast

---
+++ Financial Forecast/FinancialForecast.java
@@ -0,0 +1,24 @@
+public class FinancialForecast {
+
+    // Recursive function
+    public static double forecast(double currentValue, double growthRate, int years) {
+
+        // Base Case
+        if (years == 0)
+            return currentValue;
+
+        // Recursive Case
+        return forecast(currentValue, growthRate, years - 1) * (1 + growthRate);
+    }
+
+    public static void main(String[] args) {
+
+        double currentValue = 10000;
+        double growthRate = 0.10; //10%
+        int years = 5;
+
+        double futureValue = forecast(currentValue, growthRate, years);
+
+        System.out.printf("Forecast after %d years = %.2f", years, futureValue);
+    }
+}
